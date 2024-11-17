ca96b325d0532cdd5dcedced5dd7be8467b3f60e
Gabssanjoss <gabianjoss18@gmail.com>
2024-11-17 20:14:17 -0300

    iconAba

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,6 +7,7 @@
      <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+    <link rel="shortcut icon" type="imagex/png" href="/iconAb.ico"
     @if (file_exists(public_path('build/manifest.json')) || file_exists(public_path('hot')))
         @vite(['resources/css/app.css' , 'resources/js/app.js'])
     @else
